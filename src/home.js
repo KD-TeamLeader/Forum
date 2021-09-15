ec5acb8b7fbfba88211eff7cc1a4f346083a2ac6
KD <73846619+KD-TeamLeader@users.noreply.github.com>
2021-09-15 23:01:32 +0200

    Add files via upload

---
+++ src/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM  from "react";
+import Nav from "./nav";
+
+function Home(){
+    return(
+        <section className='window'>
+            <Nav/>
+        </section>
+    );    
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//CSS
 import './index.css';
-import App from './App';
+//Components
+import Home from './home.js';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function Wrapper(){
+  return(
+    <main className='wrapper'>
+      <Home/>
+    </main>
+  );
+}
+
+ReactDOM.render(<Wrapper />,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
